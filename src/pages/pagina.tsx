ce9275467c0eac11a46b65406e1058cd59817259
Leonardo Santos Custódio <santos-contato@hotmail.com.br>
2024-03-07 11:32:56 -0300

    Modulo 1 finalizado

--- src/pages/pagina.tsx
+++ src/pages/pagina.tsx
@@ -1,9 +1,11 @@
 import BotoesMenu from "../components/BotoesMenu";
 import Layout from "../components/Layout";
 import React, { useState, useEffect } from "react";
+import Navegador from "../components/Navegador";
+import styles from "../styles/Pagina.module.css";
 
 export default function Pagina() {
-  const [larguraTela, setLarguraTela] = useState(null);
+  const [larguraTela, setLarguraTela] = useState<number | null>(null);
 
   useEffect(() => {
     setLarguraTela(window.innerWidth);
@@ -17,13 +19,20 @@ export default function Pagina() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
+
   return (
     <Layout titulo="Pagina">
-      {larguraTela &&
+      {larguraTela !== null &&
         (larguraTela > 430 ? (
           <BotoesMenu />
         ) : (
-          <p>A tela é menor que 430px de largura.</p>
+          <div className={styles.navegadores}>
+            <Navegador destino="/estado" texto="Estado" cor="indigo" />
+            <Navegador destino="/cliente/1" texto="Cliente" />
+            <Navegador destino="/teste" texto="Teste" cor="#0f5b06" />
+            <Navegador destino="/integracao" texto="Integração" cor="#a83e02" />
+            <Navegador destino="/estatico" texto="Estatico" cor="#fa5000" />
+          </div>
         ))}
     </Layout>
   );
